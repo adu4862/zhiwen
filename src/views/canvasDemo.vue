b3c1aea7ff72077923c212551f8c97fe5d1ddd33
wangyupo <1106774262@qq.com>
2018-12-26 16:17:09 +0800

    常规提交

--- src/views/canvasDemo.vue
+++ src/views/canvasDemo.vue
@@ -3,10 +3,21 @@
         <div class="imageWrapper" ref="imageWrapper">
             <img class="real_pic" :src="dataURL"/>
             <slot>
-                <div @click="toImage">
-                    123
+                <div class="container" ref="we">
+                    <div class="top">
+                        <img src="https://b-gold-cdn.xitu.io/v3/static/img/frontend.1dae74a.png" alt="">
+                    </div>
+                    <div class="middle">
+                        welcome!
+                    </div>
+                    <div class="bottom">
+                        sdfjlasdjflj
+                    </div>
                 </div>
             </slot>
+            <div @click="toImage">
+                生成图片
+            </div>
         </div>
     </div>
 </template>
@@ -23,10 +34,13 @@
         },
         methods: {
             toImage() {
-                html2canvas(this.$refs.imageWrapper, {
+                html2canvas(this.$refs.we, {
                     backgroundColor: null,
-                    allowTaint: true,
+                    // allowTaint: true,
+                    useCORS: true,
                     dpi: window.devicePixelRatio,
+                    width: 300,
+                    // height: 480,
                 }).then((canvas) => {
                     let dataURL = canvas.toDataURL("image/png");
                     this.dataURL = dataURL;
@@ -40,6 +54,33 @@
     .canvas {
         .real_pic {
             width: 100%;
+        }
+        .container {
+            display: flex;
+            flex-flow: column;
+            position: relative;
+            margin: 0 auto;
+            width: 240px;
+            height: 500px;
+            .top {
+                img {
+                    width: 100%;
+                }
+            }
+            .middle {
+                position: absolute;
+                @include ct();
+                width: 200px;
+                height: 180px;
+                margin: 0 auto;
+                background-color: cornflowerblue;
+            }
+            .bottom {
+                display: flex;
+                align-items: flex-end;
+                flex: 1;
+                background-color: #999999;
+            }
         }
     }
 </style>
