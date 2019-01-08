6ea486db9c3d2fd98bf617cabea68f2d92e4b671
wangyupo <1106774262@qq.com>
2019-01-08 18:39:30 +0800

    my

--- src/views/my/index.vue
+++ src/views/my/index.vue
@@ -11,49 +11,49 @@
         <div class="my-cell-group">
             <div class="cell">
                 <div class="cell-left">
-                    <i class="iconfont icon-weibo"></i>
+                    <i class="icon icon--7"></i>
                     <span class="label">手机绑定</span>
                 </div>
                 <div class="cell-right">
-                    <span class="label">未验证</span>
-                    >
+                    <p class="label">未验证</p>
+                    <i class="cell-right-arrow"></i>
                 </div>
             </div>
             <div class="cell">
                 <div class="cell-left">
-                    <i class="iconfont icon-weibo"></i>
+                    <i class="icon icon--9"></i>
                     <span class="label">推广中心</span>
                 </div>
                 <div class="cell-right">
-                    <span class="label">未验证</span>
-                    >
+                    <p class="label">可提现 <span class="cell-right-withdraw">110.13元</span></p>
+                    <i class="cell-right-arrow"></i>
                 </div>
             </div>
             <div class="cell">
                 <div class="cell-left">
-                    <i class="iconfont icon-weibo"></i>
+                    <i class="icon icon-"></i>
                     <span class="label">我的订单</span>
                 </div>
                 <div class="cell-right">
-                    >
+                    <i class="cell-right-arrow"></i>
                 </div>
             </div>
             <div class="cell">
                 <div class="cell-left">
-                    <i class="iconfont icon-weibo"></i>
+                    <i class="icon icon--14"></i>
                     <span class="label">我的收藏</span>
                 </div>
                 <div class="cell-right">
-                    >
+                    <i class="cell-right-arrow"></i>
                 </div>
             </div>
             <div class="cell">
                 <div class="cell-left">
-                    <i class="iconfont icon-weibo"></i>
+                    <i class="icon icon-yue"></i>
                     <span class="label">我的余额</span>
                 </div>
                 <div class="cell-right">
-                    >
+                    <i class="cell-right-arrow"></i>
                 </div>
             </div>
         </div>
@@ -62,7 +62,6 @@
                 title="我的课程"
             />
         </div>
-
     </div>
 </template>
 
@@ -87,7 +86,9 @@
             display: flex;
             flex-flow: column;
             align-items: center;
-            background:linear-gradient(223deg,rgba(127,156,255,1) 0%,rgba(154,129,255,1) 100%);
+            background: url("../../assets/img/my/head-bg.png") no-repeat center;
+            background-size: contain;
+            background-color: #fff;
             .header {
                 margin-bottom: 15px;
                 width: 80px;
@@ -111,15 +112,33 @@
                 padding: 13px 0 10px 12px;
                 @include border-bottom();
                 &-left {
+                    .icon {
+                        display: inline-block;
+                        vertical-align: middle;
+                        font-size: $text-btn-size;
+                        color: #809CFF;
+                    }
                     .label {
-                        margin-left: 11px;
+                        margin-left: 8px;
                         color: #606060;
                     }
                 }
                 &-right {
+                    display: flex;
+                    font-size: $text-info-size;
                     .label {
                         margin-right: 9px;
                         color: #909090;
+                    }
+                    &-withdraw {
+                        color: #F09300;
+                    }
+                    &-arrow {
+                        display: inline-block;
+                        width: 8px;
+                        height: 13px;
+                        background: url("../../assets/img/my/cell-arrow.png") no-repeat center center;
+                        background-size: contain;
                     }
                 }
             }
