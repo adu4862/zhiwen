c2caf2b1a97c66e036a6d71025452af8f2b3a38f
wangyupo <1106774262@qq.com>
2019-01-09 10:04:36 +0800

    常规提交

--- src/views/my/balance/index.vue
+++ src/views/my/balance/index.vue
@@ -79,7 +79,7 @@
                 }
                 .income {
                     display: flex;
-                    justify-content: space-between;
+                    justify-content: space-around;
                     font-weight: 300;
                     .title {
                         font-size: $text-small-size;
