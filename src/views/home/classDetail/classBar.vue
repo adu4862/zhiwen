b0ffb6a00c085b464a9d6bf5f48ced4dd18285a6
wangyupo <1106774262@qq.com>
2019-01-15 23:24:27 +0800

    常规提交

---
+++ src/views/home/classDetail/classBar.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="class-bar">
+
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "classBar"
+    }
+</script>
+
+<style lang="scss" scoped>
+    .class-bar {
+        position: fixed;
+        bottom: 0;
+        left: 0;
+        right: 0;
+    }
+</style>
