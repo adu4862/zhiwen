7feb3597806e441d3873465b7b1d9cb118f898a6
wangyupo <1106774262@qq.com>
2019-01-22 12:09:54 +0800

    常规提交

--- src/views/discount/records/index.vue
+++ src/views/discount/records/index.vue
@@ -2,27 +2,20 @@
     <div class="discount-records">
         <DividerTitle class="discount-records-title">砍价记录</DividerTitle>
         <div class="discount-records-list">
-            <div class="discount-records-list-item">
-                <div class="left">
-                    <img src="" alt="" class="head-img">
-                    <div class="info">
-                        <p class="name">西门大官人</p>
-                        <p class="time">2018.11.15 15:24:55</p>
-                    </div>
-                </div>
-                <div class="right">砍掉1.76元</div>
-            </div>
+            <RecordItem />
         </div>
     </div>
 </template>
 
 <script>
     import {DividerTitle} from '@/components'
+    import RecordItem from './recordItem'
 
     export default {
         name: "index",
         components: {
-            DividerTitle
+            DividerTitle,
+            RecordItem,
         }
     }
 </script>
@@ -35,30 +28,7 @@
             color: #222222;
         }
         &-list {
-            &-item {
-                display: flex;
-                justify-content: space-between;
-                align-items: center;
-                font-size: $font-size-sm;
-                .left {
-                    display: flex;
-                    align-items: center;
-                    .head-img {
-                        width: 45px;
-                        height: 45px;
-                        border-radius: 50%;
-                    }
-                    .name {
-                        color: #606060;
-                    }
-                    .time {
-                        color: $color-gary;
-                    }
-                }
-                .right {
-                    color: #222222;
-                }
-            }
+            margin-top: 20px;
         }
     }
 </style>
---
+++ src/views/discount/records/recordItem.vue
@@ -0,0 +1,56 @@
+<template>
+    <div class="discount-records-list-item">
+        <div class="left">
+            <img :src="require('@/assets/img/demo_class_banner.png')" alt="header" class="head-img">
+            <div class="info">
+                <p class="name">西门大官人</p>
+                <p class="time">2018.11.15 15:24:55</p>
+            </div>
+        </div>
+        <div class="right">砍掉1.76元</div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "recordItem"
+    }
+</script>
+
+<style lang="scss" scoped>
+    .discount-records-list-item {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        margin-top: 10px;
+        font-size: $font-size-sm;
+        .left {
+            display: flex;
+            align-items: center;
+            .head-img {
+                width: 45px;
+                height: 45px;
+                border-radius: 50%;
+            }
+            .info {
+                margin-left: 10px;
+                .name {
+                    @include height(17px);
+                    color: #606060;
+                }
+                .time {
+                    margin-top: 3px;
+                    @include height(14px);
+                    font-size: $font-size-mini;
+                    color: $color-gary;
+                }
+            }
+        }
+        .right {
+            color: #606060;
+        }
+        &:nth-child(1) {
+            margin-top: initial;
+        }
+    }
+</style>
