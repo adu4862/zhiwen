cc5b66523bc0e1cc99a1a262470a92dd91229be2
wangyupo <1106774262@qq.com>
2019-01-24 11:12:09 +0800

    常规提交

--- src/views/home/classMedia/index.vue
+++ src/views/home/classMedia/index.vue
@@ -1,5 +1,9 @@
 <template>
     <div class="class-media">
+        <div class="class-media-video">
+            <Xgplayer :config="config" @player="Player = $event"/>
+        </div>
+        <img class="class-media-punch" :src="require('@/assets/img/icon_punch_circle.png')" alt="icon_punch">
         <Modal
             :isVisible="checkWifiVisible"
             title="继续播放将消耗流量"
@@ -13,22 +17,29 @@
 </template>
 
 <script>
+    import Xgplayer from 'xgplayer-vue'
     import {Modal} from "@/components"
     import {isWifi} from "@/common/util"
 
     export default {
         name: "classMedia",
         components: {
+            Xgplayer,
             Modal,
         },
         data() {
             return {
+                config: {
+                    id: 'vs',
+                    url: '/xgplayer-demo.mp4'
+                },
+                Player: null,
                 checkWifiVisible: false,
             }
         },
         mounted() {
             // this.checkWifi();
-            this.checkWifiVisible = true;
+            // this.checkWifiVisible = true;
         },
         methods: {
             checkWifi() {
@@ -47,5 +58,13 @@
 </script>
 
 <style lang="scss" scoped>
-
+    .class-media {
+        &-punch {
+            position: fixed;
+            right: 0;
+            bottom: 25px;
+            width: 58px;
+            height: 58px;
+        }
+    }
 </style>
