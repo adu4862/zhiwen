6e8dd5d013f20a84b2bbc543a93193babe4dc325
wangyupo <1106774262@qq.com>
2019-01-24 18:18:45 +0800

    常规提交

--- src/router/discount.js
+++ src/router/discount.js
@@ -1,10 +1,16 @@
 // 砍价页面
 const Discount = () => import( /* webpackChunkName: "home" */ '../views/discount/index')
+const Punch = () => import( /* webpackChunkName: "home" */ '../views/punch/index')
 
 export default [
     {
         path: '/discount',
         name: 'discount',
         component: Discount,
+    },
+    {
+        path: '/punch',
+        name: 'punch',
+        component: Punch,
     },
 ]
---
+++ src/views/punch/index.vue
@@ -0,0 +1,144 @@
+<template>
+    <div class="punch">
+        <div class="punch-card">
+            <div class="punch-card-head">
+                <img :src="require('@/assets/img/demo_class_banner.png')" alt="wx_header">
+                <span>用户名称</span>
+            </div>
+            <p class="punch-card-title">
+                我已连续XX天学习XXXX X课程！
+            </p>
+            <div class="punch-card-info">
+                <p class="punch-card-info-title">
+                    statute
+                </p>
+                <p class="punch-card-info-content">
+                    词汇解释词汇解释词汇解释词汇解释词汇解释词汇解释词汇解释词汇解释词汇解释词汇解释词汇解释
+                </p>
+            </div>
+            <img class="punch-card-img" :src="require('@/assets/img/punch/punch_01.png')" alt="punch_img"/>
+            <div class="punch-card-qr">
+                <img :src="require('@/assets/img/punch/demo_qr.png')" alt="qr">
+                <p>长按识别左边二维码<br/>马上加入该课程学习！</p>
+            </div>
+        </div>
+        <div class="punch-operate">
+            <div class="punch-operate-selector">
+                <p class="punch-operate-selector-title">选择皮肤</p>
+                <div class="punch-operate-selector-cards">
+                    <div class="punch-operate-selector-cards-item">
+
+                    </div>
+                </div>
+            </div>
+            <button class="blue-btn-48">保存</button>
+            <p class="punch-operate-tips">保存海报并分享朋友圈，客服查看后，即可完成当日打卡</p>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Punch"
+    }
+</script>
+
+<style lang="scss" scoped>
+    .punch {
+        @include ftb();
+        flex-flow: column;
+        padding-bottom: 15px;
+        height: 100vh;
+        background: url("~@/assets/img/punch/punch_bg.png") no-repeat center;
+        background-size: cover;
+        &-card {
+            @include ftb();
+            flex-flow: column;
+            margin-top: 13px;
+            padding: 0 30px;
+            width: 346px;
+            height: 570px;
+            background: url("~@/assets/img/punch/punch_card_bg.png") no-repeat center;
+            background-size: cover;
+            text-align: center;
+            > div {
+                width: 100%;
+            }
+            &-head {
+                @include ftb();
+                margin-top: 18px;
+                font-size: $font-size-sm;
+                color: #606060;
+                img {
+                    width: 43px;
+                    height: 43px;
+                    border-radius: 50%;
+                }
+                span {
+                    margin-left: 14px;
+                }
+            }
+            &-title {
+                margin-top: 18px;
+                @include height(25px);
+                font-size: 18px;
+                font-weight: 500;
+                color: $color-important;
+            }
+            &-info {
+                margin-top: 38px;
+                &-title {
+                    font-size: 26px;
+                    font-weight: 600;
+                    color: $color-link;
+                }
+                &-content {
+                    margin-top: 17px;
+                    line-height: 17px;
+                    font-size: $font-size-sm;
+                    color: #909090;
+                }
+            }
+            &-img {
+                margin-top: 28px;
+                width: 181px;
+                height: 151px;
+            }
+            &-qr {
+                @include ftb();
+                justify-content: center;
+                margin-top: 3px;
+                img {
+                    width: 74px;
+                    height: 74px;
+                }
+                p {
+                    margin-left: 9px;
+                    line-height: 17px;
+                    font-weight: 600;
+                }
+            }
+        }
+        &-operate {
+            font-size: $font-size-sm;
+            color: #606060;
+            &-selector {
+                margin-top: 8px;
+                padding: 11px 26px 15px 26px;
+                width: 346px;
+                background-color: #fff;
+                &-title {
+                    @include height(17px);
+                }
+            }
+            button {
+                margin-top: 20px;
+                width: 346px;
+            }
+            &-tips {
+                margin-top: 4px;
+                text-align: center;
+            }
+        }
+    }
+</style>
