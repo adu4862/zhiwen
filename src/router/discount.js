315dd6fa41d53e06172bef2483e1ea577eee6f16
wangyupo <1106774262@qq.com>
2019-01-25 11:58:39 +0800

    常规提交

--- src/router/discount.js
+++ src/router/discount.js
@@ -2,6 +2,7 @@
 const Discount = () => import( /* webpackChunkName: "home" */ '../views/discount/index')
 const Punch = () => import( /* webpackChunkName: "home" */ '../views/punch/index')
 const PunchSuccess = () => import( /* webpackChunkName: "home" */ '../views/punch/punchSuccess/index')
+const Publish = () => import( /* webpackChunkName: "home" */ '../views/publish/index')
 
 export default [
     {
@@ -18,5 +19,10 @@ export default [
         path: '/punch/success',
         name: 'punchSuccess',
         component: PunchSuccess,
+    },
+    {
+        path: '/publish',
+        name: 'publish',
+        component: Publish,
     },
 ]
