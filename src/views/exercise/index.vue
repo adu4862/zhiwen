006f32beb3fbe2fc11f0f5c1e7d4c347188d8328
wangyupo <1106774262@qq.com>
2019-02-11 11:29:31 +0800

    debug

--- src/views/exercise/index.vue
+++ src/views/exercise/index.vue
@@ -2,13 +2,14 @@
     <div class="exercise">
         <TitlePage @on-start="handleStart" v-if="status==='title'"/>
         <TopicPage v-if="status==='topic'"/>
+        <ResPage v-if="status==='res'"/>
     </div>
 </template>
 
 <script>
     import TitlePage from "./titlePage"
     import TopicPage from "./topicPage"
-    import Bar from "./bar"
+    import ResPage from "./resPage"
     import {mapState, mapMutations, mapActions, mapGetters} from 'vuex'
 
     export default {
@@ -16,11 +17,11 @@
         components: {
             TitlePage,
             TopicPage,
-            Bar,
+            ResPage,
         },
         data() {
             return {
-                status: 'title',     // title-封面,topic-联系,finish-结束
+                status: 'title',     // title-封面，topic-联系，res-结束
             }
         },
         mounted() {
