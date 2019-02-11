a1c6f54eea220e3b5f4eba319d596435a1dd399a
wangyupo <1106774262@qq.com>
2019-02-11 23:22:08 +0800

    debug

--- src/api/my.js
+++ src/api/my.js
@@ -7,21 +7,21 @@ import {sessionSetItem, sessionGetItem, isWeiXin, GetRequest} from '../common/ut
 const userId = sessionGetItem('userId');
 
 /* 用户收益 */
-export const userRewardAmount = data => Request.get('/user/' + userId + '/reward/amount', data);
+export const userRewardAmount = data => Request.get('/user/' + sessionGetItem('userId') + '/reward/amount', data);
 /* 推广订单 */
-export const userRewards = data => Request.get('/user/' + userId + '/rewards', data);
+export const userRewards = data => Request.get('/user/' + sessionGetItem('userId') + '/rewards', data);
 /* 推广订单详情 */
 export const userRewardDetail = data => Request.get('/reward/' + data.id + '/reward-record', data);
 /* 我的订单 */
-export const userOrders = data => Request.get('/user/' + userId + '/orders', data);
+export const userOrders = data => Request.get('/user/' + sessionGetItem('userId') + '/orders', data);
 /* 我的收藏 */
-export const userCollections = data => Request.get('/user/' + userId + '/collections', data);
+export const userCollections = data => Request.get('/user/' + sessionGetItem('userId') + '/collections', data);
 /* 我的收藏 - 删除 */
 export const deleteUserCollections = data => Request.delete('/collection/' + data.id);
 /* 我的课程 */
-export const userProducts = data => Request.get('/user/' + userId + '/products', data);
+export const userProducts = data => Request.get('/user/' + sessionGetItem('userId') + '/products', data);
 
 /* 打卡信息 */
-export const punchInfo = data => Request.get('/user/' + userId + '/punch', data);
+export const punchInfo = data => Request.get('/user/' + sessionGetItem('userId') + '/punch', data);
 /* 用户打卡 */
 export const punch = data => Request.post('/punch', data);
