45cb4ac72942959a272c6f97210afe360a518d4a
wangyupo <1106774262@qq.com>
2019-02-12 11:14:02 +0800

    debug

--- src/views/home/classDetail/classBar/index.vue
+++ src/views/home/classDetail/classBar/index.vue
@@ -38,22 +38,23 @@
             ...mapActions(['getWechatPayConfig', 'setOrder']),
             ...mapActions('home', ['setClassCollection']),
             init() {
-                wechatShare({
-                    url: encodeURIComponent(location.href.split('#')[0]),
-                    jsApiList: [
-                        'chooseWXPay',
-                    ]
-                }).then((res) => {
-                    let {appId, timestamp, nonceStr, signature, jsApiList} = res;
-                    wx.config({
-                        debug: false,
-                        appId: appId,
-                        timestamp,
-                        nonceStr,
-                        signature,
-                        jsApiList
-                    });
-                });
+                this.initWxConfig();
+                // wechatShare({
+                //     url: encodeURIComponent(location.href.split('#')[0]),
+                //     jsApiList: [
+                //         'chooseWXPay',
+                //     ]
+                // }).then((res) => {
+                //     let {appId, timestamp, nonceStr, signature, jsApiList} = res;
+                //     wx.config({
+                //         debug: false,
+                //         appId: appId,
+                //         timestamp,
+                //         nonceStr,
+                //         signature,
+                //         jsApiList
+                //     });
+                // });
             },
             handleDiscount() {
                 this.$router.push({name: 'discount'})
@@ -63,17 +64,16 @@
                     product_id: this.classDetail.id,
                     referrer_id: null
                 }).then((res) => {
-                    console.log(res)
                     this.getWechatPayConfig({
                         id: res.id
                     }).then((res) => {
                         let {timeStamp, nonceStr, signType, paySign} = res;
                         wx.chooseWXPay({
-                            timestamp: timeStamp, // 支付签名时间戳，注意微信jssdk中的所有使用timestamp字段均为小写。但最新版的支付后台生成签名使用的timeStamp字段名需大写其中的S字符
-                            nonceStr, // 支付签名随机串，不长于 32 位
-                            package: res.package, // 统一支付接口返回的prepay_id参数值，提交格式如：prepay_id=\*\*\*）
-                            signType, // 签名方式，默认为'SHA1'，使用新版支付需传入'MD5'
-                            paySign, // 支付签名
+                            timestamp: timeStamp,
+                            nonceStr,
+                            package: res.package,
+                            signType,
+                            paySign,
                             success: function (res) {
                                 // 支付成功后的回调函数
                                 this.$toast({
