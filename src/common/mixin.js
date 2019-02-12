17fa650f0d0f5bf441d7a1c4a4ea6fc93ecd602b
wangyupo <1106774262@qq.com>
2019-02-12 10:59:04 +0800

    debug

--- src/common/mixin.js
+++ src/common/mixin.js
@@ -47,6 +47,7 @@ export default {
         },
         // 微信分享初始化
         initWxConfig() {
+            let isDebug = true;     // 是否开启调试模式
             wechatShare({
                 url: encodeURIComponent(location.href.split('#')[0]),
                 jsApiList: [
@@ -60,7 +61,7 @@ export default {
             }).then((res) => {
                 let {appId, timestamp, nonceStr, signature, jsApiList} = res;
                 wx.config({
-                    debug: false,   // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
+                    debug: isDebug,   // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
                     appId: appId,   // 必填，公众号的唯一标识
                     timestamp,      // 必填，生成签名的时间戳
                     nonceStr,       // 必填，生成签名的随机串
