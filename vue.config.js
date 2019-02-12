67b1a0bf85c5ee9965f0837ff1c9b9d5f24d8e91
wangyupo <1106774262@qq.com>
2019-02-12 18:05:57 +0800

    debug

--- vue.config.js
+++ vue.config.js
@@ -31,7 +31,6 @@ module.exports = {
             .set('@', resolve('src'))
             .set('assets', resolve('src/assets'))
             .set('components', resolve('src/components'))
-            .set('public', resolve('public'))
     },
 
     configureWebpack: config => {
